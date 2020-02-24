1d5cdb05224ac8a0eb2de1c5761fed728e8bef9b
sheldon <mouzc@morningtec.jp>
2020-02-25 07:34:07 +0800

    Initial commit

---
+++ src/main/java/com/dao/TeacherMapper.java
@@ -0,0 +1,16 @@
+package com.dao;
+
+import com.entity.Teacher;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface TeacherMapper {
+    public List<Teacher> selectAllTeacher();
+
+    public int insertTeacher(Teacher teacher);
+
+    public int updateTeacher(Teacher teacher);
+}
